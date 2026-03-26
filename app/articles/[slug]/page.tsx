10a6d19736942a4dda544d0e4d2c68d5a7fa6f65
Agnès Gresset <agnes.gresset@brandung.de>
2026-03-26 12:49:38 +0100

    VD-002: temp deactivated article page

--- app/articles/[slug]/page.tsx
+++ app/articles/[slug]/page.tsx
@@ -2,20 +2,20 @@ import { InfoMessage } from '@/components/ui/InfoMessage';
 import { Headline } from '@/ui/Typography';
 import { Suspense } from 'react';
 
-type ArticlePageProps = {
-  params: Promise<{ slug: string }>;
-}
+// type ArticlePageProps = {
+//   params: Promise<{ slug: string }>;
+// }
 
 // TODO: PLACEHOLDER FOR NOW 
-export default async function ArticlePage({ params }: ArticlePageProps) {
-  // TODO: do we need this?
-  const { slug } = await params;
-  const title = decodeURIComponent(slug).replace(/-/g, ' ');
+export default async function ArticlePage(/*{ params }: ArticlePageProps*/) {
+  // // TODO: do we need this?
+  // const { slug } = await params;
+  // const title = decodeURIComponent(slug).replace(/-/g, ' ');
 
   return (
     <article className="mx-auto max-w-3xl py-8">
       <Suspense fallback={<InfoMessage type="loading" message="Loading article…" />}>
-        <Headline styleAs="h1">{title}</Headline>
+        <Headline styleAs="h1">Article Title</Headline>
       </Suspense>
     </article>
   );
